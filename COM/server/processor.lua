1ab6df19146eece7303359d10edd454c3efc0d44
Amit Agnani <cputerminator@hotmail.com>
2014-01-19 23:14:22 +0800

    Fixed PLAYERINFO command
    The player id requested is now prepended to the response to notify the
    caller that this player no longer exists (and to distinguish it from a
    invalid request).

--- COM/server/processor.lua
+++ COM/server/processor.lua
@@ -124,7 +124,7 @@ function Processor.process(request, ReqID)
 			if p ~= nil then
 				return GeneratePlayerInfo(p)
 			else
-				return "null"
+				return request[3].." null"
 			end
 		elseif command == "PLAYERDATA" then
 			local playerdata = ""
