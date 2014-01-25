e5e98f29b5246a96e1757d96a4a22f7d1a1e79b4
Amit Agnani <cputerminator@hotmail.com>
2014-01-25 15:09:36 +0800

    Removed extra semicolon
    Removed extra semicolon and improved formatting.

--- COM/server/globals.lua
+++ COM/server/globals.lua
@@ -1,13 +1,13 @@
 function string.starts(String,Start)
-    return string.sub(String,1,string.len(Start))==Start
+    return string.sub(String,1,string.len(Start)) == Start
 end
 
 function string.ends(String,End)
-    return End=='' or string.sub(String,-string.len(End))==End
+    return End == '' or string.sub(String,-string.len(End)) == End
 end
 
 function string.split(String, delimiter)
-    result = {};
+    result = {}
     for match in (String..delimiter):gmatch("(.-)"..delimiter) do
         table.insert(result, match);
     end
