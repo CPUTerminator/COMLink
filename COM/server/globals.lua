2b203a43b3fcc36891d061d307af6bb0823cde8f
Amit Agnani <cputerminator@hotmail.com>
2014-01-29 18:19:18 +0800

    Added function base64enc()
    Added a base 64 encoding function for transmitting string data that
    could contain delimiting characters such as whitespace and commas.
    
    This should allow for the transport of general strings such as server
    name and description across the network.

--- COM/server/globals.lua
+++ COM/server/globals.lua
@@ -1,3 +1,5 @@
+local base64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
 function string.starts(String,Start)
     return string.sub(String,1,string.len(Start)) == Start
 end
@@ -21,6 +23,32 @@ end
 
 function toboolean(v)
     return (type(v) == "string" and v == "true") or (type(v) == "number" and v ~= 0) or (type(v) == "boolean" and v)
+end
+
+function base64enc(data)
+    return ((data:gsub('.', 
+        function(x) 
+            local r,base64='', x:byte()
+            for i=8,1,-1 do
+                r = r..(base64 % 2^i - base64 % 2^(i-1) > 0 and '1' or '0')
+            end
+
+            return r
+        end
+    )..'0000'):gsub('%d%d%d?%d?%d?%d?', 
+        function(x)
+            if (#x < 6) then
+                return ''
+            end
+
+            local c = 0
+            for i = 1,6 do
+                c = c + (x:sub(i,i) == '1' and 2^(6-i) or 0)
+            end
+
+            return base64:sub(c+1,c+1)
+        end
+    )..({'', '==', '='})[#data % 3+1])
 end
 
 function supports(feature)
